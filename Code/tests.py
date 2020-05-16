5177d93903ebe5caa3e9a982a7d4da5e61639370
Gabe Corsini <gabe.corsini@gmail.com>
2020-05-16 09:38:19 -0400

    still a scratchpad

--- Code/tests.py
+++ Code/tests.py
@@ -1,43 +1,17 @@
-# Python OOP tutorial from Corey Schafer on Youtube
+import openpyxl
 
+wb = openpyxl.load_workbook('automate_online-materials/example.xlsx')
+sheet = wb.active
+print(sheet['A1'])
+print(sheet['A1'].value)
+c = sheet['B1']
+print(c.value)
 
-class Employee:
+print('Row {}, Column {} is {}.'.format(str(c.row), c.column, c.value))
+print('Cell {} is {}.'.format(c.coordinate, c.value))
 
-    num_of_employees = 0
-    raise_amount = 1.05
+print(sheet.cell(row=1, column=2))
+print(sheet.cell(row=1, column=2).value)
 
-    def __init__(self, first, last, pay):
-        self.first = first.capitalize()
-        self.last = last.capitalize()
-        self.pay = pay
-        self.email = ('{}.{}@bjorncorp.com'.format(first, last))
-
-        Employee.num_of_employees += 1
-
-    def fullname(self):
-        return '{} {}'.format(self.first, self.last)
-
-    def apply_raise(self):
-        self.pay = int(self.pay * self.raise_amount)
-
-    @classmethod
-    def set_raise_amt(cls, amount):
-        cls.raise_amount = amount
-
-    @classmethod
-    def from_string(cls, emp_str):
-        first, last, pay = emp_str.split('-')
-        return cls(first, last, pay)
-
-    # @staticmethod
-    # def is_workday(day):
-    #     if day == datetime.date.weekday(5) or day == datetime.date.weekday(6):
-    #         return False
-    #     else:
-    #         return True
-
-
-print('Enter employee info (first name, last name, pay) separated by "-": ')
-emp_str = input()
-emp_1 = Employee.from_string(emp_str)
-print(emp_1.fullname(), emp_1.email, emp_1.pay)
+for i in range(1, 8):
+    print(i, sheet.cell(row=i, column=2).value)
